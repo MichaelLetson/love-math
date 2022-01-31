d45c2baf735a632fde288ad4bfc091afe73da4ef
MichaelLetson <mletsongamble@gmail.com>
2022-01-31 19:23:08 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,25 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('You clicked Submit!');
         } else {
             let gameType = this.getAttribute('data-type');
-            alert(`You clicked ${gameType}`);
+            runGame(gameType);
            }
         })
     }
+
+    runGame('addition');
 })
 
-function runGame() {
+
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAddtionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -34,8 +44,10 @@ function incrementWrongAnswers() {
 
 }
 
-function displayAddtionQuestion() {
-
+function displayAddtionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
